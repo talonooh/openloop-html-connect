109206ae971ad00a1465fc21cdef99765cbfcfa8
Andres Amor <andres.amor@q.media>
2018-01-16 15:44:30 -0300

    1749: As GV, I want to deploy Waitrose HTML5 campaign through OLv3 with initial SDK. Description: - Added VSCode settings for debug unit tests. - Setted node env for unit testing so it is simple to mock window.location.href. - Added more tests depending on the state of window.location.href. - Code formatted. - Added reset method for internal use and clear variables cache, specially for testing purposes.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,47 +1,123 @@
 const openLoopConnect = require('../');
 
 describe('openLoopConnect', () => {
-  describe('getSyncPath', () => {
-    it('should return ./ by default', () => {
-      expect(openLoopConnect.getSyncPath()).toBe('./');
-    });
+	const sampleLiveCampaignUrl = 'https://stgdp.openloop.it/QDOT/UK/HTML5/DirectPanel/onetime/index.html';
 
-    describe('when setting default using setDefaultSyncPath', () => {
-      beforeEach(() => {
-        openLoopConnect.setDefaultSyncPath('myPath');
-      });
+	const setLocationHref = (url) => {
+		window.location.href = url;
+	};
 
-      it('should return the value setted', () => {
-        expect(openLoopConnect.getSyncPath()).toBe('myPath');
-      });
-    });
+	beforeEach(() => {
+		global.window = {
+			location: {
+				href: ''
+			}
+		};
+		openLoopConnect.reset();
+	});
 
-    describe('when setting using setSyncPath', () => {
-      beforeEach(() => {
-        openLoopConnect.setSyncPath('myFinalPath');
-      });
+	describe('getSyncPath', () => {
+		it('should return ./ by default', () => {
+			expect(openLoopConnect.getSyncPath()).toBe('./');
+		});
 
-      it('should return the value setted', () => {
-        expect(openLoopConnect.getSyncPath()).toBe('myFinalPath');
-      });
-    });
-  });
+		describe('when using the creative from openloop servers and it is a live campaign', () => {
+			beforeEach(() => {
+				setLocationHref(sampleLiveCampaignUrl);
+			});
 
-  describe('getFrameId', () => {
-    it('should return null if there is no query string', () => {
-      expect(openLoopConnect.getFrameId()).toBeNull();
-    });
-  });
+			it('should return ../sync/', () => {
+				expect(openLoopConnect.getSyncPath()).toBe('../sync/');
+			});
 
-  describe('isLive', () => {
-    it('should return false on local environment', () => {
-      expect(openLoopConnect.isLive()).toBeFalsy();
-    });
-  });
+			describe('and despite the defaultSyncPath is setted', () => {
+				beforeEach(() => {
+					openLoopConnect.setDefaultSyncPath('myPath');
+				});
 
-  describe('isDebug', () => {
-    it('should return false by default', () => {
-      expect(openLoopConnect.isDebug()).toBeFalsy();
-    });
-  });
+				it('should return ../sync/', () => {
+					expect(openLoopConnect.getSyncPath()).toBe('../sync/');
+				});
+			});
+		});
+
+		describe('when setting default using setDefaultSyncPath', () => {
+			beforeEach(() => {
+				openLoopConnect.setDefaultSyncPath('myPath');
+			});
+
+			it('should return the value setted', () => {
+				expect(openLoopConnect.getSyncPath()).toBe('myPath');
+			});
+		});
+
+		describe('when setting using setSyncPath', () => {
+			beforeEach(() => {
+				openLoopConnect.setSyncPath('myFinalPath');
+			});
+
+			it('should return the value setted', () => {
+				expect(openLoopConnect.getSyncPath()).toBe('myFinalPath');
+			});
+		});
+	});
+
+	describe('getFrameId', () => {
+		it('should return null if there is no query string', () => {
+			expect(openLoopConnect.getFrameId()).toBeNull();
+		});
+
+		describe('when using frame_id on the query string', () => {
+			beforeEach(() => {
+				setLocationHref(sampleLiveCampaignUrl + '?frame_id=12345');
+			});
+
+			it('should return correct value', () => {
+				expect(openLoopConnect.getFrameId()).toBe('12345');
+			});
+		});
+
+		describe('when using player_id on the query string', () => {
+			beforeEach(() => {
+				setLocationHref(sampleLiveCampaignUrl + '?player_id=123456789');
+			});
+
+			it('should return correct value', () => {
+				expect(openLoopConnect.getFrameId()).toBe('123456789');
+			});
+		});
+	});
+
+	describe('isLive', () => {
+		it('should return false on local environment', () => {
+			expect(openLoopConnect.isLive()).toBeFalsy();
+		});
+
+		describe('when using the creative from openloop servers and it is a live campaign', () => {
+			beforeEach(() => {
+				setLocationHref(sampleLiveCampaignUrl);
+			});
+
+			it('should return true', () => {
+				expect(openLoopConnect.isLive()).toBeTruthy();
+			});
+		});
+
+	});
+
+	describe('isDebug', () => {
+		it('should return false by default', () => {
+			expect(openLoopConnect.isDebug()).toBeFalsy();
+		});
+
+		describe('when setting the debug flag on the query string', () => {
+			beforeEach(() => {
+				setLocationHref(sampleLiveCampaignUrl + '?frame_id=12345&debug=1');
+			});
+
+			it('should return true', () => {
+				expect(openLoopConnect.isDebug()).toBeTruthy();
+			});
+		});
+	});
 });
